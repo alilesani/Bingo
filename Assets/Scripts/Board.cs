dd0bd5b4852b548ba0adde39f14b8edcbf825a37
James <James@google.com>
2023-08-01 13:11:20 +0330

    Adding a function to generate an array of diiferent random numbers for a given range.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] _cells;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[] RangeRandomGenerator(int count, int start, int finish)
     {
-        
+        int[] result = new int[count];
+        int rnd;
+        do
+        {
+            rnd = UnityEngine.Random.Range(start, finish);
+            result[count - 1] = rnd;
+        } while (count > 0 && !Array.Exists(result, el => el == rnd));
+        return result;
     }
+
+
 }
